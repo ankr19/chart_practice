91a45a2948d960a32dc844cf9048ab40e10a513a
anand kumar <ankm335@gmail.com>
2024-04-06 21:24:37 +0530

    practices

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
+"use client"
 import Image from "next/image";
 import LineChart from "@/components/LineChart";
+import { sendMail } from "@/constants/EmailSending";
 
 export default function Home() {
+  const handleClick = async()=> {
+    // await sendMail();
+  }
+
   return (
     <>
     <LineChart/>
+    <button onClick={()=>handleClick()} className="bg-green-500 text-white px-3 py-2 capitalize cursor-pointer hover:bg-gray-300 hover:text-green-500 border-transparent rounded-md m-10">
+      send mail
+    </button>
     </>
   );
 }
---
+++ components/Email.jsx
@@ -0,0 +1,14 @@
+"use client"
+import * as React from 'react';
+import { Html } from '@react-email/html';
+import { Button } from '@react-email/button';
+
+export function Email(props) {
+  const { url } = props;
+
+  return (
+    <Html lang="en">
+      <Button href={url}>Click me</Button>
+    </Html>
+  );
+}
