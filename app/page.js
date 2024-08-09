3322b5872ffc061072a10e28e93bc21d22be4d6b
anand kumar <ankm335@gmail.com>
2024-08-09 15:52:41 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,22 +1,25 @@
-"use client"
+"use client";
 import Image from "next/image";
 import LineChart from "@/components/LineChart";
 import PieChartG from "@/components/PieChart";
 import LineChart2 from "@/components/LineChart2";
 
 export default function Home() {
-  const handleClick = async()=> {
+  const handleClick = async () => {
     // await sendMail();
-  }
+  };
 
   return (
     <>
-    <LineChart/>
-    <button onClick={()=>handleClick()} className="bg-green-500 text-white px-3 py-2 capitalize cursor-pointer hover:bg-gray-300 hover:text-green-500 border-transparent rounded-md m-10">
-      send mail
-    </button>
-    <PieChartG/>
-    <LineChart2/>
+      <LineChart />
+      <button
+        onClick={() => handleClick()}
+        className="bg-green-500 text-white px-3 py-2 capitalize cursor-pointer hover:bg-gray-300 hover:text-green-500 border-transparent rounded-md m-10"
+      >
+        send mail
+      </button>
+      <PieChartG />
+      <LineChart2 />
     </>
   );
 }
